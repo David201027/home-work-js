3f2c40bbeada7d438a6fa2ca6d40d6f5a4daae11
David201027 <156106393+David201027@users.noreply.github.com>
2024-12-04 19:53:43 +0200

    hw-3

--- lesson-3/js/lesson-3.js
+++ lesson-3/js/lesson-3.js
@@ -1,1 +1,10 @@
 console.log('Lesson-3 JS');
+// Створення змінних
+let myFavoriteActor = "Sean Connery"; // Ваш улюблений актор
+let male = "my favorite actor's name"; // Опис: актор або актриса
+
+// Отримання останнього символу
+let lastCharacter = myFavoriteActor.slice(-1).toUpperCase(); // Беремо останній символ та перетворюємо його на велику літеру
+
+// Виведення результату в консоль
+console.log(`The last letter in ${male} ${myFavoriteActor} is '${lastCharacter}'!`);
